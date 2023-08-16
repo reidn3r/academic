b61981272544b40bdfb971bfa11c33cbdc971413
reidn3r <reidnersteam@hotmail.com>
2023-08-16 12:04:21 -0300

    pagina de perfil

--- src/controllers/getProfileController.js
+++ src/controllers/getProfileController.js
@@ -6,21 +6,24 @@ const sequelize = require('../config/sequelizeConfig');
 const getProfile = async(req, res) => {
     const { id } = req.params;
 
-    let foundUser = await userModel.findOne({where: {id: id}})|| await companyModel.findOne({where: {id: id}});
+    let foundUser = await userModel.findOne({where: {register_id: id}})|| await companyModel.findOne({where: {register_id: id}});
     if(!foundUser) return res.status(404).json({message: "User not found"});
     
-    const [foundProfile, metadata] = await sequelize.query(`SELECT * FROM profile WHERE contact_email="${foundUser.email}" LIMIT 1`);
-    if(!foundProfile) return res.status(404).json({message: "Profile not found"});
-
+    // return res.json({profile: foundUser});
+    
+    const [foundProfile, metadata] = await sequelize.query(`SELECT * FROM profile WHERE register_id=${foundUser.register_id} LIMIT 1`);
+    if(foundProfile.length == 0) return res.status(404).json({message: "Profile not found"});
+    
     const foundImage = await imageModel.findByPk(foundProfile[0].image_id);
-
+    
     const profileName = foundProfile[0].name;
     const profileEmail = foundProfile[0].contact_email;
     const profileDesc = foundProfile[0].description;
     const profileImage = foundImage.image_data;
 
     const context = { profileName, profileEmail, profileDesc, profileImage };
-    res.json({profile: context});
+    // return res.json({profile: context});
+    return res.render('profile', {context});
 }
 
 module.exports = getProfile;
