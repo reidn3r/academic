7d0c59ee679064d512deca20e26f6e0c0bdb0c41
reidn3r <reidnersteam@hotmail.com>
2023-08-18 16:08:10 -0300

    renderizacao dos contatos add.

--- src/controllers/getProfileController.js
+++ src/controllers/getProfileController.js
@@ -2,6 +2,7 @@ const userModel = require('../model/User');
 const companyModel = require('../model/Company');
 const imageModel = require('../model/Image_Info');
 const topicsModel = require('../model/Topics_Interest');
+const contactModel = require('../model/Contact_type');
 const sequelize = require('../config/sequelizeConfig');
 const jwt = require('jsonwebtoken');
 
@@ -27,13 +28,22 @@ const getProfile = async(req, res) => {
     let topics = [];
     if(profileTopics.length > 0){
         profileTopics.forEach(async(topic_id) => {
-            // console.log(topic_id);
             const foundTopic = await topicsModel.findOne({ where: {id: topic_id.topic_id}});
             topics.push(foundTopic.topic);
-            console.log('ok')
         })
     }
 
+    let contacts_array = [];
+    const [profileContacts, contact_metadata] = await sequelize.query(`SELECT contact_content, contact_type_id FROM profile_contacts WHERE profile_id=${foundProfile[0].id}`);
+    if(profileContacts.length > 0){
+        profileContacts.forEach(async(contact) => {
+            let typeContact = await contactModel.findOne({ where: {id: contact.contact_type_id}});
+
+            let content = contact.contact_content;
+            let typeContactName = typeContact.type; 
+            contacts_array.push({ content, typeContactName });
+        })
+    }
     
     const foundImage = await imageModel.findByPk(foundProfile[0].image_id);
     const profileName = foundProfile[0].name;
@@ -42,7 +52,8 @@ const getProfile = async(req, res) => {
     const profileImage = foundImage.image_data;
     const mimeType = foundImage.image_content_type;
     
-    const context = { profileName, profileEmail, profileDesc, profileImage, mimeType, topics, auth };
+    console.log(contacts_array);
+    const context = { profileName, profileEmail, profileDesc, profileImage, mimeType, topics, contacts_array, auth };
 
     // return res.json({profile: context});
     return res.render('profile', {context});
