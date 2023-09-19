fb155b45e310a9e78df9b3d012e9c129eb99660f
reidn3r <reidnersteam@hotmail.com>
2023-09-19 09:26:54 -0300

    query.js: alteracao

--- src/public/utils/query.js
+++ src/public/utils/query.js
@@ -15,32 +15,6 @@
         campo_1="valor_1" AND campo_2="valor_2" AND ...
 */
 
-// const query = (data) => {
-//     let select = "SELECT register_id ";
-//     let from = "FROM user WHERE ";
-    
-//     const fields = Object.keys(data);
-//     const values = Object.values(data);
-
-//     for(let j=0; j<fields.length; j++){
-//         if(values[j]){
-//             if(typeof(values[j]) === "string"){
-//                 if(fields[j] === "name"){
-//                     from += `${fields[j]} LIKE "%${values[j]}%"`;
-//                 }
-//                 else{
-//                     from += `${fields[j]}="${values[j]}"`;
-//                 }
-//             }
-//             else{
-//                 from += `${fields[j]}=${values[j]}`;
-//             }
-//             if(j <= fields.length - 2) from += " AND ";
-//             if(j == fields.length - 1) from += "; ";
-//         }
-//     }
-//     return select + from;
-// }
 const query = (data) => {
     let select = "SELECT * FROM profile AS p INNER JOIN user AS u ON p.register_id=u.register_id ";
     let from = "WHERE ";
@@ -62,7 +36,6 @@ const query = (data) => {
                 from += `u.${fields[j]}=${values[j]}`;
             }
             if(j <= fields.length - 2) from += " AND ";
-            // if(j == fields.length - 1) from += "; ";
         }
     }
     return select + from;
