ef210f99cf9a9344f4fbf834484710ee920709fd
reidn3r <reidnersteam@hotmail.com>
2023-09-21 11:09:28 -0300

    correcao: limitacao da quantidade maxima de imagens selecionadas/renderizadas em tela

--- src/public/utils/loadImage.js
+++ src/public/utils/loadImage.js
@@ -1,12 +1,22 @@
+let lastImages = [];
 
 const loadImage = (e) => {
+    const maxFiles = 3;
     const imageContainer = document.querySelector('.loaded-images');
     imageContainer.style.display = "flex";
 
-    for(let i=0; i<e.target.files.length; i++){
-        let image = document.createElement('img');
-        image.className = 'uploaded-image'
-        image.src = URL.createObjectURL(e.target.files[i])
-        imageContainer.appendChild(image)
+    imageContainer.innerHTML = '';
+    lastImages = [];
+
+    for(let i = 0; i < maxFiles; i++){
+        if (e.target.files[i]) {
+            let image = document.createElement('img');
+            image.className = 'uploaded-image';
+            image.src = URL.createObjectURL(e.target.files[i]);
+            imageContainer.appendChild(image);
+            
+            // Armazena as últimas imagens selecionadas na variável global
+            lastImages.push(e.target.files[i]);
+        }
     }
 }
