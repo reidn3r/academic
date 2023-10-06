82c8303c09b77a2fbc3893061e61a8a2d99e3916
reidn3r <reidnersteam@hotmail.com>
2023-10-06 16:50:04 -0300

    fix: bug na criacao de perfil

--- src/controllers/api/api.createProfile.js
+++ src/controllers/api/api.createProfile.js
@@ -9,8 +9,8 @@ const dateFormat = require('../../public/utils/dateFormat');
 const createProfile = async(req, res, next) => {
     try{
         const session = req.session.profile_data;
-        const entity_data = session.profileData;    
-            
+        const entity_data = session.profileData;
+        
         const { profileDesc } = req.body;
         if(!entity_data) return res.redirect('/v1/login');
         if(!profileDesc) return res.status(400).json({message: "Profile description required."});
@@ -24,19 +24,20 @@ const createProfile = async(req, res, next) => {
             blob = fs.readFileSync(path.join(__dirname, '..', '..', 'public', 'images', 'default_user.png'));
             fileMimetype = 'Image/png';
         }
-    
+
+        
         //salva img. do perfil no bd
         const newImage = await imageModel.create({
             image_data: blob,
             image_content_type: fileMimetype,
             created_at: dateFormat(new Date())
         });
-    
+
         //salva novo perfil no bd
         const newProfile = await profileModel.create({
-            register_id: entity_data.register_id,
-            name: entity_data.name,
-            contact_email: entity_data.email,
+            register_id: entity_data.newUser.register_id,
+            name: entity_data.newUser.name,
+            contact_email: entity_data.newUser.email,
             description: profileDesc,
             created_at: dateFormat(new Date()),
             updated_at: dateFormat(new Date()),
